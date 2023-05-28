40ee0ae6a5ce459025c2dbbef53cf761597cc5ac
hieu1966hn <hieu1966hn@gmail.com>
2023-05-28 11:00:18 +0700

    New code For in, For of

---
+++ SS3/ss3.js
@@ -0,0 +1,23 @@
+//////////// Duyệt mảng?
+let arr = [1, 2, 3, 5, 6, 7, 8, 87, 5, 34, 5, 35, 546, 67, 7, 8, 23, 12, 3];
+
+///// In ra từng phần tử mảng lên màn hình console.log
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+
+///// For in
+// for (let i in arr) {
+//   // console.log(i);
+//   console.log(arr[i]);
+// }
+
+/*
+i: vị trí phần tử của mảng
+*/
+
+// for(let key of arr ){
+//    console.log(key);
+// }
+////// => in ra phần tử mang 
