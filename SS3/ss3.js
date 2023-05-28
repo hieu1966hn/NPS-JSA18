c2910f45b98a023a37ee28619913e73ed5b064cd
hieu1966hn <hieu1966hn@gmail.com>
2023-05-28 11:35:03 +0700

    new code Object

--- SS3/ss3.js
+++ SS3/ss3.js
@@ -21,3 +21,43 @@ i: vị trí phần tử của mảng
 //    console.log(key);
 // }
 ////// => in ra phần tử mang 
+
+
+
+
+////// Giới thiệu về đối tượng (Object) trong JS
+/// Khai báo đối tượng
+let obj = {
+  /// Thuộc tính
+  name: "Nguyễn Trung Hiếu",
+  age: 25,
+  gender: "male",
+  cccd: "0123456789",
+  hobbies: ["sleep", "code", "movie"],
+
+  ///// Phương thức
+  say: function () {
+    alert("Hello, How are you?")
+  }
+}
+console.log(obj.name);
+obj.say();
+
+/*
+Object khi nào sử dụng?
+*/
+
+///// Thêm mới một thuộc tính: 
+obj.job = "dev";
+obj.name = "Hiếu"
+
+//// Xóa 1 thuộc tính
+delete obj.job;
+
+
+/// Thêm mới một phương thức
+obj.code = function () {
+  alert("I am Coder");
+}
+
+obj.code();
