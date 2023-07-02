0addc2b53ea10dff2682218589f4c4a6fdc2a547
hieu1966hn <hieu1966hn@gmail.com>
2023-07-02 10:25:02 +0700

    new code SS8

---
+++ SS7_8/js/index.js
@@ -0,0 +1,15 @@
+
+const DEFAULT_VALUE = '__';
+const searchInput = document.getElementById("search-input");
+const cityName = document.querySelector(".city-name");
+const weatherState = document.querySelector(".weather-state");
+const weatherIcon = document.querySelector("weather-icon");
+const temperature = document.querySelector(".temperature");
+
+///////Section 2
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
+
+
